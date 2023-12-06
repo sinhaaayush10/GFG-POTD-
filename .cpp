fd5d14b86a629b295bc1ce129a7337ac98c91957
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-12-06 12:42:18 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int countX( int l, int r, int x ) {
+    int c{};
+    while ( ++l < r ) {
+        int t{ l };
+        do if ( t % 10 == x ) ++c;
+        while ( t /= 10 );
+    }
+    return c;
+}
+};

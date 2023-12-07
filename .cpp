383a8251258c633150e4cc8d3c1b9c2ce81563d9
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-12-07 14:12:10 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,73 @@
+class Solution{
+
+    public:
+
+    long countSubarrays(int a[], int n, int L, int R)
+
+    {
+
+        long count =0;
+
+        long size =0;
+
+        //counting the cases where the maximum element is smaller than or equal to R
+
+        for(int i =0;i< n;i++){
+
+            if(a[i] <=R){
+
+                while(a[i] <=R and i < n){
+
+                    size++;
+
+                    i++;
+
+                }
+
+            }
+
+            count+=(size*(size + 1))/2;
+
+            if(a[i] > R){
+
+                size =0;
+
+            }
+
+        }
+
+        //removing the cases where the maximum element is less than L 
+
+        size =0;
+
+        for(int i =0;i< n;i++){
+
+            if(a[i] < L){
+
+                while(a[i] < L and i < n){
+
+                    size++;
+
+                    i++;
+
+ 
+
+                } 
+
+       }
+
+            count-=(size*(size + 1))/2;
+
+            if(a[i] >=L){
+
+                size =0;
+
+            }
+
+        }
+
+        return count;
+
+    }
+
+};

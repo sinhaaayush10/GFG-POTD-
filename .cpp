90f41a59dfd28f8c60dd860dcf347c01c7f1759e
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-12-19 14:57:31 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+  int posOfRightMostDiffBit(int m, int n)
+    {
+        for(int i=0; i<31; i++){
+            int j = (1<<i);
+            if((m&j)!=(n&j)) return i+1;
+        }
+        
+        return -1;
+    }
+};

66e3f420b7ee46deb88312cb59ad0d8cdacb36f9
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2023-12-25 22:54:28 +0530

    Update

---
+++ .cpp
@@ -0,0 +1,34 @@
+class Solution
+{   
+    public:
+    //Function for finding determinant of matrix.
+    int determinantOfMatrix(vector<vector<int>> &matrix, int n)
+    {
+        if(n == 1)
+            return matrix[0][0];
+            
+        int ans = 0;
+        
+        // traverse all columns of 0th row
+        for(int i = 0; i < n; i++){
+            vector<vector<int>> second(n - 1, vector<int> (n - 1));
+            
+            // traverse all rows of second matrix
+            for(int j = 1; j < n; j++){
+                int x = 0;
+                
+                // traverse all columns of second matrix
+                for(int k = 0; k < n; k++){
+                    if(k == i)
+                        continue;
+                        
+                    second[j - 1][x++] = matrix[j][k];
+                }
+            }
+            
+            ans += matrix[0][i] * determinantOfMatrix(second, n - 1) * ((i & 1) ? -1 : 1);
+        }
+        
+        return ans;
+    }
+};

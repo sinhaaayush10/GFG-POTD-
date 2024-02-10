26119ff0e21290a6abd5d07a8fded488f57cfdff
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-10 11:35:03 +0530

    Update Number of paths in a matrix with k coins.cpp

--- Number of paths in a matrix with k coins.cpp
+++ Number of paths in a matrix with k coins.cpp
@@ -4,7 +4,7 @@ public:
     long long dp[101][101][101];
     
     long long solve(int x, int y,int n, int k, vector<vector<int>>& arr){
-        if(x >= n or y >= n or k<0) return 0ll;
+        if(x >= n or y >= n or k<0) return 0ll; 
         if(x == n-1 and y == n-1 and k - arr[x][y] == 0) return 1ll;
         if(dp[k][x][y]!=-1) return dp[k][x][y];
         return  dp[k][x][y] = solve(x+1,y,n,k-arr[x][y],arr) + solve(x,y+1,n,k-arr[x][y],arr); 

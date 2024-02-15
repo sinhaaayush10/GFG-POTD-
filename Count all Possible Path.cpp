5e4ba48cf320e62f8cf75bc8e29634f9578dac27
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-15 19:06:33 +0530

    Update Count all Possible Path.cpp

--- Count all Possible Path.cpp
+++ Count all Possible Path.cpp
@@ -3,7 +3,7 @@ public:
 int isPossible(vector<vector<int>>paths){
     for(int i=0;i<paths.size();i++) {
         int c = 0;
-        for(int j=0;j<paths.size();j++){
+        for(int j=0;j<paths.size();j++){n 
             if(paths[i][j]==1) c++;
         }
         if(c%2) return 0;

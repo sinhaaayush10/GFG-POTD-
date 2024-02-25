2cf9b860025a4f42fdadbcdc584a86727db45010
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-25 17:10:56 +0530

    Update Reach a given score.cpp

--- Reach a given score.cpp
+++ Reach a given score.cpp
@@ -6,7 +6,7 @@ class Solution
     long long int count(long long int n) 
         vector<int>dp(n+1,0);
         dp[0] = 1;
-        
+         
         for(int i=3;i<=n;i++){
             dp[i] +=dp[i-3];
         }

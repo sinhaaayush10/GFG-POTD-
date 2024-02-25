2dcd067d2453dccf8ea318efb7d17a6f7f2b22ba
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-02-25 17:10:35 +0530

    Update Reach a given score.cpp

--- Reach a given score.cpp
+++ Reach a given score.cpp
@@ -3,8 +3,7 @@ class Solution
 {
     public:
     // Complete this function
-    long long int count(long long int n)
-    {
+    long long int count(long long int n) 
         vector<int>dp(n+1,0);
         dp[0] = 1;
         

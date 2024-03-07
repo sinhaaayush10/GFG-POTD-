0a380472dc5c40d51eacc8b898e65454402c9d24
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-03-07 22:48:54 +0530

    Longest repeating and non-overlapping substring.cpp

--- Longest repeating and non-overlapping substring.cpp
+++ Longest repeating and non-overlapping substring.cpp
@@ -8,7 +8,7 @@ class Solution {
         
         for(int i=1;i<n+1;i++)
         {
-            for(int j=1;j<n+1;j++)
+            for(int j=1;j<n+1;j++)m 
             {
                 if(str[i-1] == str[j-1] && i!=j)
                 {

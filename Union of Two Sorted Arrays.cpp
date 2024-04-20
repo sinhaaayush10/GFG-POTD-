0c855e1426205932d14e8de7a2df8131ba04dd49
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-04-20 19:06:37 +0530

    \Union of Two Sorted Arrays.cpp

--- Union of Two Sorted Arrays.cpp
+++ Union of Two Sorted Arrays.cpp
@@ -2,7 +2,7 @@ class Solution{
     public:
     //arr1,arr2 : the arrays
     // n, m: size of arrays
-    //Function to return a list containing the union of the two arrays. 
+    //Function to return a list containing the union of the two arrays.  
     vector<int> findUnion(int arr1[], int arr2[], int n, int m)
     {
         //Your code here

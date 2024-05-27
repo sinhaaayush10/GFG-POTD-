dcf6af044d39bc0872badb9fe455bc273a2b6a0f
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-05-27 17:11:33 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubseq(int n, int[] a) {
+        HashMap<Integer, Integer> dp = new HashMap<>();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int current = a[i];
+            int longestSubsequenceLength = Math.max(dp.getOrDefault(current - 1, 0), dp.getOrDefault(current + 1, 0)) + 1;
+            dp.put(current, longestSubsequenceLength);
+            ans = Math.max(ans, longestSubsequenceLength);
+        }
+        return ans;
+    }
+}

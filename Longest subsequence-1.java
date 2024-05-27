00965181af72e5853b10bf53ab8a79ae31868c81
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-05-27 17:11:55 +0530

    Update Longest subsequence-1.java

--- Longest subsequence-1.java
+++ Longest subsequence-1.java
@@ -2,7 +2,7 @@ class Solution {
     public int longestSubseq(int n, int[] a) {
         HashMap<Integer, Integer> dp = new HashMap<>();
         int ans = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { 
             int current = a[i];
             int longestSubsequenceLength = Math.max(dp.getOrDefault(current - 1, 0), dp.getOrDefault(current + 1, 0)) + 1;
             dp.put(current, longestSubsequenceLength);

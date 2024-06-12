69f2602d8537dbb2cdb810b51338fa41a56f1053
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-06-12 23:15:53 +0530

    Count numbers containing 4.cpp

---
+++ Count numbers containing 4.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        // code here
+         int count=0;
+        for(int i=0;i<=n;i++){
+            if(to_string(i).find("4")!=-1){
+                count++;
+            }
+        }
+        return count;
+    }
+};

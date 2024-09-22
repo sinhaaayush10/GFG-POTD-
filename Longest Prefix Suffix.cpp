712664c31592270f369575350ad7ee5bc255ca20
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-09-22 13:50:41 +0530

    Longest Prefix Suffix.cpp

---
+++ Longest Prefix Suffix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    public:
+    int lps(string str) {
+        vector<int> A(str.size(), 0);
+        int j = 0, i = 1;
+        while (i < str.size()) {
+            if (str[i] == str[j]) {
+                A[i] = j + 1;
+                j++;
+                i++;
+            } 
+            else {
+                if (j == 0)
+                    i++;
+                else
+                    j = A[j - 1];
+            }
+        }
+        return A.back();
+    }
+};

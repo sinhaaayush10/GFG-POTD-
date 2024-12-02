4e00927a926a2a963ada60568139b5ac18129570
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2024-12-02 19:28:20 +0530

    Search Pattern (KMP-Algorithm)

---
+++ Search Pattern (KMP-Algorithm)I.cpp
@@ -0,0 +1,53 @@
+class Solution {
+  public:
+    void computeLPSArray(string pat, int M, vector<int>& lps) {
+        int len = 0;
+        int i;
+
+        lps[0] = 0;
+        i = 1;
+        while (i < M) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    vector<int> search(string& pat, string& txt) {
+        vector<int> res;
+        int M = pat.size();
+        int N = txt.size();
+        vector<int> lps(M + 1, 0);
+        int j = 0;
+        computeLPSArray(pat, M, lps);
+        int f = 0;
+        int i = 0; // index for txt[]
+        while (i < N) {
+            if (pat[j] == txt[i]) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                f++;
+                res.push_back(i - j);
+                j = lps[j - 1];
+            }
+            else if (i < N && pat[j] != txt[i]) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+        return res;
+    }
+};

c1ebb111b153f1d63502730d191fbd9cae33d4b2
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2025-01-25 22:10:24 +0530

    Initial Commit

---
+++ Find the first node of loop in linked list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    Node* findFirstNode(Node* head) {
+        Node* slow = head;
+        Node* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};

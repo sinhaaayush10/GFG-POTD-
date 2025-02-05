297adb196280b9046e17dd8008f493c8e3f2a078
Aayush Sinha <128314119+sinhaaayush10@users.noreply.github.com>
2025-02-05 23:16:40 +0530

    Initial Commit

---
+++ Mirror Tree l.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    void mirror(Node* node) {
+        if (node == NULL)
+            return;
+        mirror(node->left);
+        mirror(node->right);
+        swap(node->left, node->right);
+    }
+};
